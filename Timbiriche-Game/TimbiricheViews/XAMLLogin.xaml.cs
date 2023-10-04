4c1e55c5cb92fde2e93f643aa90407c246b67fde
Cesar Emiliano Lezama Lopez <zS21013857@estudiantes.uv.mx>
2023-10-04 16:46:18 -0600

    Log in View completed

---
+++ Timbiriche-Game/TimbiricheViews/XAMLLogin.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimbiricheViews
+{
+    public partial class XAMLLogin : Window
+    {
+        public XAMLLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void changeLanguage()
+        {
+            String language = "es";
+            if (lbLanguage.Content.Equals("Español"))
+            {
+                language = "en";
+            }
+
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(language);
+            XAMLLogin newWindow = new XAMLLogin();
+            if (language.Equals("en"))
+            {
+                newWindow.lbLanguage.Content = "English";
+            }
+            this.Close();
+            newWindow.Show();
+        }
+
+        private void onClickChangeLanguage(object sender, MouseButtonEventArgs e)
+        {
+            changeLanguage();
+        }
+    }
+}
