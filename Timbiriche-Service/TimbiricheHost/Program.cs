e6c9cc5bc1e324aa748fa57463c897d80731b60c
Eduardo García Díaz <eduardo.gd17@gmail.com>
2023-10-04 17:11:08 -0600

    DataAccess, Host and UserManagerService ADDED

--- Timbiriche-Service/TimbiricheHost/Program.cs
+++ Timbiriche-Service/TimbiricheHost/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,12 @@ namespace TimbiricheHost
     {
         static void Main(string[] args)
         {
+            using (ServiceHost host = new ServiceHost(typeof(TimbiricheService.UserManagerService)))
+            {
+                host.Open();
+                Console.WriteLine("Server is running");
+                Console.ReadLine();
+            }
         }
     }
 }
