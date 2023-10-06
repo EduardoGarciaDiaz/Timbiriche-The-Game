ee38317fb8b15b4f7175d29517112888f5afe5a5
Cesar Emiliano Lezama Lopez <zS21013857@estudiantes.uv.mx>
2023-10-05 20:33:18 -0600

    Validate Credentials in Service

--- Timbiriche-Service/TimbiricheDataAccess/UserManagement.cs
+++ Timbiriche-Service/TimbiricheDataAccess/UserManagement.cs
@@ -8,13 +8,26 @@ namespace TimbiricheDataAccess
 {
     public class UserManagement
     {
-        public void AddUser(Accounts account, Players player)
+        public void addUser(Accounts account, Players player)
         {
             using (var context = new TimbiricheDBEntities())
             {
                 var newAccount = context.Accounts.Add(account);
                 var newPlayer = context.Players.Add(player);
                 context.SaveChanges();
+            }
+        }
+
+        public bool validateCredentials(String username, String password)
+        {
+            using(var context = new TimbiricheDBEntities())
+            {
+                var query = from player in context.Players
+                            where player.username == username && player.password == password
+                            select player;
+                bool userExists = query.Any();
+
+                return userExists;
             }
         }
     }
--- Timbiriche-Service/TimbiricheService/IUserManager.cs
+++ Timbiriche-Service/TimbiricheService/IUserManager.cs
@@ -9,61 +9,64 @@ using System.Threading.Tasks;
 namespace TimbiricheService
 {
 
-        [ServiceContract]
-        public interface IUserManager
-        {
-            [OperationContract]
-            int AddUser(Account account, Player player);
-        }
+    [ServiceContract]
+    public interface IUserManager
+    {
+        [OperationContract]
+        int addUser(Account account, Player player);
 
-        [DataContract]
-        public class Account
-        {
-            private int _idAccount;
-            private string _name;
-            private string _lastName;
-            private string _surname;
-            private DateTime _birthdate;
-
-            [DataMember]
-            public int IdAcccount { get { return _idAccount; } set { _idAccount = value; } }
-            [DataMember]
-            public string Name { get { return _name; } set { _name = value; } }
-            [DataMember]
-            public string LastName { get { return _lastName; } set { _lastName = value; } }
-            [DataMember]
-            public string Surname { get { return _surname; } set { _surname = value; } }
-            [DataMember]
-            public System.DateTime Birthdate { get { return _birthdate; } set { _birthdate = value; } }
-
-        }
-
-        [DataContract]
-
-        public class Player
-        {
-            private int _idPlayer;
-            private string _username;
-            private string _email;
-            private string _password;
-            private int _coins;
-            private string _status;
-            private Account _accountFK;
-
-            [DataMember]
-            public int IdPlayer { get { return _idPlayer; } set { _idPlayer = value; } }
-            [DataMember]
-            public string Username { get { return _username; } set { _username = value; } }
-            [DataMember]
-            public string Email { get { return _email; } set { _email = value; } }
-            [DataMember]
-            public string Password { get { return _password; } set { _password = value; } }
-            [DataMember]
-            public int Coins { get { return _coins; } set { _coins = value; } }
-            [DataMember]
-            public string Status { get { return _status; } set { _status = value; } }
-            [DataMember]
-            public Account AccountFK { get { return _accountFK; } set { _accountFK = value; } }
-
-        }
+        [OperationContract]
+        bool validateCredentials(String username, String password);
     }
+
+    [DataContract]
+    public class Account
+    {
+        private int _idAccount;
+        private string _name;
+        private string _lastName;
+        private string _surname;
+        private DateTime _birthdate;
+
+        [DataMember]
+        public int idAcccount { get { return _idAccount; } set { _idAccount = value; } }
+        [DataMember]
+        public string name { get { return _name; } set { _name = value; } }
+        [DataMember]
+        public string lastName { get { return _lastName; } set { _lastName = value; } }
+        [DataMember]
+        public string surname { get { return _surname; } set { _surname = value; } }
+        [DataMember]
+        public System.DateTime birthdate { get { return _birthdate; } set { _birthdate = value; } }
+
+    }
+
+    [DataContract]
+
+    public class Player
+    {
+        private int _idPlayer;
+        private string _username;
+        private string _email;
+        private string _password;
+        private int _coins;
+        private string _status;
+        private Account _accountFK;
+
+        [DataMember]
+        public int idPlayer { get { return _idPlayer; } set { _idPlayer = value; } }
+        [DataMember]
+        public string username { get { return _username; } set { _username = value; } }
+        [DataMember]
+        public string email { get { return _email; } set { _email = value; } }
+        [DataMember]
+        public string password { get { return _password; } set { _password = value; } }
+        [DataMember]
+        public int coins { get { return _coins; } set { _coins = value; } }
+        [DataMember]
+        public string status { get { return _status; } set { _status = value; } }
+        [DataMember]
+        public Account accountFK { get { return _accountFK; } set { _accountFK = value; } }
+
+    }
+}
