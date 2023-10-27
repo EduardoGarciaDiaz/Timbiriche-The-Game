c8f2094a95f274adddf26c62e03cb14b646734a8
Cesar Emiliano Lezama Lopez <zS21013857@estudiantes.uv.mx>
2023-10-27 12:26:18 -0600

    Model Change

--- Timbiriche-Service/TimbiricheDataAccess/Accounts.cs
+++ Timbiriche-Service/TimbiricheDataAccess/Accounts.cs
@@ -25,7 +25,6 @@ namespace TimbiricheDataAccess
         public string lastName { get; set; }
         public System.DateTime birthdate { get; set; }
         public string surname { get; set; }
-        public string status { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Players> Players { get; set; }
