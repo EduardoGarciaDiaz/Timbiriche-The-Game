8455feddd56c8195464c496404a83f8b946d2366
Cesar Emiliano Lezama Lopez <emilianolezama@outlook.com>
2023-12-10 06:44:46 -0600

    First test for ShopManagement

---
+++ Timbiriche-Service/TimbiricheTests/UnitTestsDataAccess/ShopManagementTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimbiricheDataAccess;
+using Xunit;
+
+
+namespace TimbiricheTests.UnitTestsDataAccess
+{
+    public class ShopManagementTest
+    {
+        [Fact]
+        public void TestGetColorsSuccess()
+        {
+            List<Colors> colors = ShopManagement.GetColors();
+
+            Assert.NotNull(colors);
+            Assert.True(colors.Count > 0);
+        }
+    }
+}
